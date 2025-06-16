661db6361fc8587e1e02f7c5ec305aa1b3570017
suthakaranburaj <suthakaranburaj@gmail.com>
2025-06-17 01:48:24 +0530

    changes

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import { BackgroundBeamsWithCollision } from './ui/background-beams-with-collision'
-import { FloatingDockDemo } from './navbar/Navbar_new'
-import Hero from './hero_section/hero'
-export default {
-  BackgroundBeamsWithCollision,
-  FloatingDockDemo,
-  Hero
-}
