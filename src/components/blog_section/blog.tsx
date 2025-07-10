857fdeee2488098dd9786a5aae062b2870ac9e64
suthakaranburaj <suthakaranburaj@gmail.com>
2025-07-10 17:02:25 +0530

    error removed

--- src/components/blog_section/blog.tsx
+++ src/components/blog_section/blog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { IconCalendar, IconClock, IconArrowRight } from "@tabler/icons-react";
 import GridBackground from "../ui/grid-background";
