a153385137d3a296ef2f7162b64a326664670aa9
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-26 12:00:04 +0530

    resume updated

--- src/components/hero_section/hero.tsx
+++ src/components/hero_section/hero.tsx
@@ -331,7 +331,7 @@ function Hero() {
                   }`}
                 >
                   <Link
-                    href="/Suthakar Anburaj_new (2).pdf"
+                    href="/Suthakar Anburaj_new (3).pdf"
                     className="flex items-center justify-center gap-2"
                   >
                     <IconFileCv size={20} />
